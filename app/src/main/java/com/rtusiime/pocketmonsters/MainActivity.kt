2ecc352565bb5a346fa18844ac83bc44f9e5972b
rtusiime <rtusiime@u.rochester.edu>
2021-04-28 20:27:09 -0400

    Layout stuff

--- app/src/main/java/com/rtusiime/pocketmonsters/MainActivity.kt
+++ app/src/main/java/com/rtusiime/pocketmonsters/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_PocketMonsters)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
